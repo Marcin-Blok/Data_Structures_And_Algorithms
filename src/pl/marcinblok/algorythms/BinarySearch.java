39ff563ee0a46e561654f15e358be78349bfa56f
Marcin-Blok <marcinblok7980@gmail.com>
2021-10-02 10:33:05 +0200

    Implementation of binary search of the element in sorted array if exist, if not return -1

---
+++ src/pl/marcinblok/algorythms/BinarySearch.java
@@ -0,0 +1,27 @@
+package pl.marcinblok.algorythms;
+
+public class BinarySearch {
+
+    static int findIndexWithBinarySearch(int[] array, int target) {
+        int left = array[0];
+        int right = array.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (target < array[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] sortedArray = {-7, 2, 5, 9, 14, 27, 33, 99, 237};
+        int target = 33;
+        System.out.println(findIndexWithBinarySearch(sortedArray, target));
+    }
+}
